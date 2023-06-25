5fc82b9b920ce3be11daad7eea53f498d939ecf6
josephguscott <joseph.guscott@gmail.com>
2023-06-25 17:52:42 +0100

    feat: give ML tree score and location

--- src/generate_initial_trees.py
+++ src/generate_initial_trees.py
@@ -7,14 +7,13 @@ from utils import writeFile
 
 def generateInitialTrees(initial_software: str, msa_path: str, number_initial_trees: int) -> list:
     initial_trees = []
+    
+    parsimony_command = generateMPBootCommand(initial_software, msa_path)
 
-    if initial_software is "mpboot":
-        parsimony_command = generateMPBootCommand(initial_software, msa_path)
-
-        for tree in range(0, number_initial_trees):
-            os.system(parsimony_command)
-            initial_tree = readFile(msa_path + ".treefile")
-            initial_trees.append(initial_tree)
+    for tree in range(number_initial_trees):
+        os.system(parsimony_command)
+        initial_tree = readFile(msa_path + ".treefile")
+        initial_trees.append(initial_tree)
 
     return initial_trees
 
--- src/refine_trees.py
+++ src/refine_trees.py
@@ -4,8 +4,18 @@ from iqtree import iqtreeLikelihoodAnalysis
 
 def refineInitialTrees(msa_path:str):
     treefile = msa_path + ".treefile"
+    logfile = msa_path + ".log"
+
     refine_tree_command = iqtreeLikelihoodAnalysis(msa_path)
-    
     os.system(refine_tree_command)
 
-    print("Refined ML tree written to", treefile)
+    with open(logfile, "r") as fp:
+        lines = fp.readlines()
+        for line in lines:
+            if line.startswith("BEST SCORE FOUND : "):
+                output = line
+
+    best_score = output.split(": ")
+
+    print("")
+    print("Refined ML tree score: {}Refined ML treefile: {}".format(best_score[-1], treefile))
