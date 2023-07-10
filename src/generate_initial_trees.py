3efce7b07722257a63e232db7513a87036548455
josephguscott <joseph.guscott@gmail.com>
2023-07-10 10:13:41 +0100

    feat: remove arguments

--- src/generate_initial_trees.py
+++ src/generate_initial_trees.py
@@ -18,10 +18,12 @@ def generateInitialTrees(initial_software: str, msa_path: str, number_initial_tr
     #    initial_tree = readFile(msa_path + ".treefile")
     #    initial_trees.append(initial_tree)
 
+    # generate initial trees in parallel 
     with Pool() as pool:
-        items =[(parsimony_command, msa_path, i) for i in range(number_initial_trees)]
+        items =[(parsimony_command, i) for i in range(number_initial_trees)]
         pool.starmap(parallelGenerateTrees, items)
 
+    # concatenate trees into a single file
     for tree_number in range(number_initial_trees):
         command = "cat tree." + str(tree_number) + ".treefile >> parsimony.treefile"
         os.system(command)
@@ -34,7 +36,7 @@ def writeInitialTrees(initial_trees: list):
     for i in range(len(initial_trees)):
         writeFile("initial_trees.treefile", initial_trees[i])
 
-def parallelGenerateTrees(parsimony_command, msa_path, tree_number):
+def parallelGenerateTrees(parsimony_command: str, tree_number: int):
     loop_parsimony_command = parsimony_command + " -pre tree." + str(tree_number)
     loop_parsimony_command = loop_parsimony_command + " > /dev/null 2>&1"
     os.system(loop_parsimony_command)
