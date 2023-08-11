84386a34c21f09ccd4c4aa9759bef8e422b03853
josephguscott <joseph.guscott@gmail.com>
2023-08-11 10:26:12 +0100

    feat: skeleton parallelisation

--- src/iqtree.py
+++ src/iqtree.py
@@ -3,7 +3,7 @@ def iqtreeEvaluateTreesCommand(msa_path: str, cores: int) -> str:
         pass_msa = "-s " + msa_path
         pass_treefile = " -z initial_trees.treefile"
         no_search = " -n 0"
-        parallel = " -nt " + cores
+        parallel = " -nt " + str(cores)
 
         evaulate_tree_command = iqtree_path + pass_msa + pass_treefile + no_search + parallel
         evaulate_tree_command = evaulate_tree_command + " > /dev/null 2>&1"
@@ -16,7 +16,7 @@ def iqtreeLikelihoodAnalysis(msa_path: str, cores: int) -> str:
     iqtree_path = "./lib/iqtree "
     pass_msa = "-s " + msa_path
     pass_treefile = " -t initial_trees_best.treefile"
-    parallel = " -nt " + cores
+    parallel = " -nt " + str(cores)
 
     likelihood_command = iqtree_path + pass_msa + pass_treefile + parallel
     likelihood_command = likelihood_command + " > /dev/null 2>&1"
