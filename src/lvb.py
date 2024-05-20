2891e2df9d0ebf4168567fadf37dbc3a866221a4
David Bunn <davbunn1@gmail.com>
2024-05-20 11:26:17 +0000

    Added file format detection for lvb

--- src/lvb.py
+++ src/lvb.py
@@ -1,9 +1,21 @@
 # Written by David Bunn, 2024 (modified from mpboot.py)
 # Generates LVB command for Maximum Parsimony stage
+import re
 
 def GenerateLVBCommand(initial_software: str, msa_path: str) -> str:
     software_path = f'{initial_software} '
     pass_msa_path = "-i " + msa_path
-    command = software_path + pass_msa_path
 
+    file_extension = re.search(".(\w+)\Z", msa_path)
+    formats = {'phy':'phylip','ph':'phylip','phylip':'phylip',
+               'fa':'fasta','fasta':'fasta',
+               'nex':'nexus','nxs':'nexus','nexus':'nexus',
+               'aln':'clustal','clustal':'clustal'}
+    if file_extension:
+        msa_format = formats[file_extension[1].lower()]
+        print(msa_format.upper() + ' format detected.')
+    else: msa_format = 'phylip'
+
+    command = software_path + pass_msa_path + ' -f ' + msa_format
+    
     return command
